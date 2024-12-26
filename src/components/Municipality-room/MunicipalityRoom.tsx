509760afccfd5724db23446980ae98ea5cb9bc04
graciusyengo <yengogracius1996@gmail.com>
2024-12-27 00:36:34 +0100

    feat: make name of municipality

--- src/components/Municipality-room/MunicipalityRoom.tsx
+++ src/components/Municipality-room/MunicipalityRoom.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./MunicipalityRoom.css"
 import { BiHomeSmile } from 'react-icons/bi'
 import { FaMartiniGlassEmpty, FaPersonChalkboard } from 'react-icons/fa6'
 import { HiOutlineCake } from 'react-icons/hi2'
@@ -6,38 +7,39 @@ import { ImManWoman } from 'react-icons/im'
 import { LiaUniversitySolid } from 'react-icons/lia'
 import { MdOutlineFestival } from 'react-icons/md'
 
+
 function MunicipalityRoom() {
   return (
    <div className="container-municipality-room">
-         <h2 className="title-municilpality">Quelle salle recherchez-vous ?</h2>
+         <h2 className="title-municilpality">Location des salles par commune ?</h2>
          <div className="cards">
            <div className="card">
              <ImManWoman  size={45} className="manWoman"/>
-             <div>Salle de Mariage</div>
+             <div>Makala</div>
            </div>
            <div className="card">
              <MdOutlineFestival size={45} className="outlineFestical"/>
-             <div>Salle de Réception</div>
+             <div>Gombe</div>
            </div>
            <div className="card">
              <HiOutlineCake size={45} className="outlineCake" />
-             <div>Salle de D'anniversaire</div>
+             <div>Lemba</div>
            </div>
            <div className="card">
              <LiaUniversitySolid  size={45} className="universitySolid" />
-             <div>Salle Universitaire</div>
+             <div>Matete</div>
            </div>
            <div className="card">
              <FaMartiniGlassEmpty size={45} className="martiniGlassEmpty" />
-             <div>Salle de Fêtes</div>
+             <div>Ngaliema</div>
            </div>
            <div className="card">
              <BiHomeSmile size={45}  className="homeSmile"/>
-             <div>Château Mariage</div>
+             <div>Masina</div>
            </div>
            <div className="card">
              <FaPersonChalkboard  size={45} className="personneChalkboard" />
-             <div>Salle Séminaire</div>
+             <div>Limete</div>
            </div>
          </div>
        </div>
